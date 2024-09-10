cecf4d43da3230ec0612ee9f5604af57f87ad1be
daysla2629 <daysla.silva@escola.pr.gov.br>
2024-09-10 12:16:04 -0300

    /

---
+++ aleatorio.js
@@ -0,0 +1,8 @@
+const  nomes  =  [ "Fernanda" ,  "Giuliana" ,  "Maria Eduarda" ,  "Marcelo" ,  "Amanda" ,  "Gustavo" ,  "Gabriel" ] ;
+
+ função  de exportação aleatória  ( lista ) {
+    const  posicao  =  Math . floor ( Math . random ( ) *  lista . length ) ;
+    retornar  lista [ posição ] ;
+}
+
+exportar  const  nome  =  aleatório ( nomes )
